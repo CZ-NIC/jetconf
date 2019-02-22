356c2f41f5b1e3eb74e760dda30f0a79a45f78e4
Ladislav Lhotka <lhotka@nic.cz>
2019-02-22 15:38:35 +0100

    Use full email address for client certificates

--- utils/cert_gen/gen_client_cert.sh
+++ utils/cert_gen/gen_client_cert.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$1" = "" ]; then
-	echo "Usage:" $0 "out_filename_prefix"
+	echo "Usage:" $0 "email_address"
 	exit 1
 fi
 
@@ -8,13 +8,13 @@ echo -e "\n1. Generating private key:"
 openssl genrsa -out $1.key 2048
 
 echo -e "\n2. Generating CSR:"
-openssl req -new -key $1.key -out $1.req -subj "/CN=Test/emailAddress=$1@mail.cz"
+openssl req -new -key $1.key -out $1.req -subj "/CN=Test/emailAddress=$1"
 
 echo -e "\n3. Signing CSR with test CA's key:"
 openssl x509 -req -in $1.req -CAcreateserial -CA ca.pem -CAkey ca.key -days 3650 -out $1.pem
 rm $1.req
 
-echo -e "\n4. Creating $1_curl.pem (concaterated certificate and key for curl etc.):"
+echo -e "\n4. Creating $1_curl.pem (certificate & key):"
 cat $1.pem > $1_curl.pem
 cat $1.key >> $1_curl.pem
 
